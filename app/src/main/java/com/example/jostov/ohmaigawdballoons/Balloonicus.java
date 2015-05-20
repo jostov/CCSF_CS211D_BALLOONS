2b2f37aacfe1a00fb1fa4756916e6d4027e84314
jostov <tentaclemoose@gmail.com>
2015-05-20 02:31:40 -0700

    Fixed it up and made it a little bit less psychotic. Even less psychotic now.

--- app/src/main/java/com/example/jostov/ohmaigawdballoons/Balloonicus.java
+++ app/src/main/java/com/example/jostov/ohmaigawdballoons/Balloonicus.java
@@ -11,35 +11,35 @@ import android.widget.FrameLayout;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class WHAT_AM_I_GONNA_DO_WITH_ALL_THESE_BALLOONS extends Activity{
+public class Balloonicus extends Activity{
     int height = -1;
     int width = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_what__am__i__gonna__do__with__all__these__balloons);
+        setContentView(R.layout.balloonicus);
         final FrameLayout fl = (FrameLayout)findViewById(R.id.balloonSpace);
-        fl.addView(new BALLOONS(this), 0);
+        fl.addView(new Balloon(this), 0);
         Button b1 = (Button) findViewById(R.id.addBalloon);
         b1.setOnClickListener(new View.OnClickListener() {
+            Balloon b = (Balloon) fl.getChildAt(0);
             @Override
             public void onClick(View v) {
-                BALLOONS b = (BALLOONS)fl.getChildAt(0);
                 b.addBalloon();
                 b.invalidate();
             }
         });
         Button b2 = (Button) findViewById(R.id.popBalloons);
         b2.setOnClickListener(new View.OnClickListener() {
+            Balloon b = (Balloon) fl.getChildAt(0);
             @Override
             public void onClick(View v) {
-                BALLOONS b = (BALLOONS) fl.getChildAt(0);
-                b.POP_ALL_THE_BALLOONS_OH_NO_PARTY_IS_OVER_NOW();
+                b.popBalloons();
                 b.invalidate();
             }
         });
     }
-    protected int getHeight(){
+    protected int getDimens(){
         if (height <= 0){
             height = findViewById(R.id.balloonSpace).getHeight();
         }
@@ -51,39 +51,38 @@ public class WHAT_AM_I_GONNA_DO_WITH_ALL_THESE_BALLOONS extends Activity{
         }
         return width;
     }
-    private class BALLOONS extends View
+    private class Balloon extends View
     {
-        private Random random;
-        private Paint paint;
-        private ArrayList<int[]> balloons = new ArrayList<>();//LET THE GAMES BEGIN!
-        public BALLOONS(Context context){
+        private Random random = new Random();
+        private Paint paint = new Paint();
+        private ArrayList<int[]> balloons = new ArrayList<>();
+        public Balloon(Context context){
             super(context);
-            random = new Random();
         }
         @Override
         protected void onDraw(Canvas canvas){
             super.onDraw(canvas);
-            paint = new Paint();
             for (int[] each : balloons){
                 drawBALLOON(canvas, each);
             }
         }
         protected int[] gimmeBALLOON(){
-            int[] BALLOON = new int[4];
-            BALLOON[0] = random.nextInt(200);
-            BALLOON[1] = BALLOON[0] + random.nextInt(WHAT_AM_I_GONNA_DO_WITH_ALL_THESE_BALLOONS.this.getHeight()- 2  * BALLOON[0]);
-            BALLOON[2] = BALLOON[0] + random.nextInt(WHAT_AM_I_GONNA_DO_WITH_ALL_THESE_BALLOONS.this.getWidth()-2 * BALLOON[0]);
-            BALLOON[3] = random.nextInt(255255255);
-            return BALLOON;
+            int[] ball = new int[4];
+            ball[0] = random.nextInt(200);
+            ball[1] = ball[0] + random.nextInt(getHeight()- 2  * ball[0]);
+            ball[2] = ball[0] + random.nextInt(getWidth()-2 * ball[0]);
+            ball[3] = random.nextInt(255255255);
+            return ball;
         }
-        protected void drawBALLOON(Canvas canvas, int[] BALLOON){
-            paint.setColor(Color.argb(255, (BALLOON[3] % 1000), ((BALLOON[3]/1000) % 1000), (BALLOON[3]/1000000)%1000));
-            canvas.drawCircle(BALLOON[2], BALLOON[1], BALLOON[0], paint);
+        protected void drawBALLOON(Canvas canvas, int[] ball){
+            paint.setColor(Color.argb(255, (ball[3] % 1000),
+                    ((ball[3]/1000) % 1000), (ball[3]/1000000)%1000));
+            canvas.drawCircle(ball[2], ball[1], ball[0], paint);
         }
         public void addBalloon(){
             balloons.add(gimmeBALLOON());
         }
-        public void POP_ALL_THE_BALLOONS_OH_NO_PARTY_IS_OVER_NOW(){
+        public void popBalloons(){
             balloons = new ArrayList<>();
         }
     }
